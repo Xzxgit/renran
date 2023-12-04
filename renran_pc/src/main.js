0dd188acb1324f091aa7fc8aa20c86032bd49dbc
xzx <x239375@163.com>
2023-12-04 02:02:35 -0800

    初始化前端页面

--- renran_pc/src/main.js
+++ renran_pc/src/main.js
@@ -2,20 +2,30 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router/index'
-import settings from "./settings"
+import router from './router'
+
+Vue.config.productionTip = false;
+// 加载项目的全局变量和全局方法
+import settings from "./settings";
+Vue.prototype.$settings = settings;
+
 // elementUI 导入
 import ElementUI from 'element-ui';
 import "element-ui/lib/theme-chalk/index.css";
-// 引入初始化样式
-import "../static/css/reset.css";
-
 // 调用插件
 Vue.use(ElementUI);
 
-Vue.config.productionTip = false
+// 引入初始化样式
+import "../static/css/reset.css";
+// 全局导入字体图标
+import "../static/css/iconfont.css";
+import "../static/css/iconfont.eot";
 
-Vue.prototype.$settings = settings;
+// 初始化axios
+// import axios from "axios"
+// 允许ajax发送请求时附带cookie，设置为不允许
+// axios.defaults.withCredentials = false;
+// Vue.prototype.$axios = axios; // 把对象挂载vue中
 
 /* eslint-disable no-new */
 new Vue({
@@ -24,3 +34,4 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+// 不能在new Vue后面写任何代码，系统是无法执行到这里来的。
--- renran_pc/src/router/index.js
+++ renran_pc/src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
-import Home from "../components/Home"
 
 Vue.use(Router)
+
+import Home from "../components/Home"
+// import Login from "../components/Login"
+
 
 export default new Router({
   mode: "history",
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // },
     {
-       name:"Home",
-       path:"/home",
-       component:Home,
+      path: '/',
+      name: "Home",
+      component: Home,
     },
+    // {
+    //   path: '/login',
+    //   name: "Login",
+    //   component: Login,
+    // },
   ]
 })
